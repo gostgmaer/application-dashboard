043061e8e1769fc2461bbfe64c0924414d58dce5
Kishor <kishor81160@gmail.com>
2025-09-28 21:14:26 +0530

    add

--- src/config/setting.js
+++ src/config/setting.js
@@ -1,7 +1,3 @@
-export const containerId = process.env.NEXT_PUBLIC_CONTAINER
-export const tableId = process.env.NEXT_PUBLIC_TABLE
-export const contactTable = "contacts"
-export const mongourl = process.env.MONGODB_URI
 export const baseurl = process.env.NEXT_PUBLIC_BASE_URL
 export const appUrl = process.env.NEXT_PUBLIC_APP_URL
 export const secret = process.env.NEXT_PUBLIC_SECRET
