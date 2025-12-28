379228a665f667654596c358bdc22864b38293b4
gostgmaer <kishor81160@gmail.com>
2025-12-29 00:14:32 +0530

    added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import PrivateLayout from "@/components/layout/dashboard";
 import DashboardLayout from "@/components/pages/dashboard";
 
 export default async function Page(props: any) {
-
   return (
     <PrivateLayout>
       <div className=" mx-auto py-2">
